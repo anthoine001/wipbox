eba2efc0d74777d7e8a794602adeb05b17ccc5dd
anthoine001 <antoineselosse@air-de-antoine.home>
2016-01-23 22:31:27 +0100

    test interface boutons avec code couleur

--- test_bouton.py
+++ test_bouton.py
@@ -5,14 +5,25 @@ try:
     from tkinter import  *
 except:
     from Tkinter import  *
+def nombreBouton(event):
+	a=int(s.get())
+	i=0
+	for i in range(a):
+		Button(fenetre, text =i,height=5).pack(side=LEFT)
 
 fenetre = Tk()
+#s = Spinbox(fenetre, from_=0, to=10)
+#s.pack()
+#s.bind('<Button>', nombreBouton)
 
-s = Spinbox(fenetre, from_=0, to=10)
-s.pack()
-
-Button(fenetre, text ='Bouton 1',height=50).pack(side=LEFT)
-Button(fenetre, text ='Bouton 2',height=50).pack(side=LEFT)
-
+taille =8
+orange =4
+for i in range(taille):
+		if i<orange:
+			Button(fenetre, text =i,height=5,bg="green").pack(side=LEFT)
+		elif i==orange:
+			Button(fenetre, text =i,height=5,bg="orange").pack(side=LEFT)
+		elif i>orange:
+			Button(fenetre, text =i,height=5,bg="red").pack(side=LEFT)
 
 fenetre.mainloop()
