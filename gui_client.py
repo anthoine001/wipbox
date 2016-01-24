377a4aa748147d05e0d1587800535532d8734f22
Clement Lemaire <lemairec02@gmail.com>
2016-01-24 22:59:34 +0100

    client for server

--- gui_client.py
+++ gui_client.py
@@ -5,6 +5,8 @@ try:
     from tkinter import  *
 except:
     from Tkinter import  *
+import socket
+
 
 
 #Définition de la taille de l ecran et du stock
@@ -14,6 +16,8 @@ largeur =800
 hauteur=600
 taille=largeur/(stock+1)
 niveau =0
+serveur_ip = ""
+serveur_port = 1111
 
 #initialisation de la fenêtre
 fenetre = Tk()
@@ -38,6 +42,12 @@ for i in range(stock+1):
     canvas.create_text(taille*(0.5+i),hauteur/2,justify=CENTER,text=i,font="arial 18 bold")
 canvas.pack()
 
+
+def send_niveau(niveau):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((serveur_ip, serveur_port))
+    s.send("niveau " + str(niveau))
+
 #Détection du clic, de la position et positionnement d'un symbole
 def touche(event):
     if hauteur/2-taille/2<event.y<hauteur/2+taille/2:
@@ -46,5 +56,6 @@ def touche(event):
         print(k)
         canvas.coords(marqueur,taille*k,hauteur/2+taille*0.5,taille*(k+1),hauteur/2+taille*1.5)
         niveau=k
+        send_niveau(niveau)
 canvas.bind("<Button-1>", touche)
 fenetre.mainloop()
