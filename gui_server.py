19dfd83cf47b48ac8bcba49574abd049c672ef35
anthoine001 <a-selosse@orange.fr>
2016-03-11 10:19:18 +0100

    Ajout de la config graphique

--- gui_server.py
+++ gui_server.py
@@ -13,6 +13,15 @@ import time
 
 class ServerGui():
     def __init__(self):
+        # position en x des carrés de couleur
+        self.xbox=195
+        # position en x des noms de machines
+        self.xtext = 75
+        # position en x des valeurs d'encours
+        self.xwip = 150
+        # position en x du plan
+        self.xplan = 460
+        
         #Definition de la taille de l ecran
         self.root = Tk()
         self.largeur = self.root.winfo_screenwidth()-10
@@ -25,9 +34,9 @@ class ServerGui():
         menubar = Menu(self.root)
         menu1 = Menu(menubar, tearoff=0)
         menu1.add_command(label="Initialiser les machines", command=self.initMachine)
-        menu1.add_command(label="Editer", command=self.alert)
+        menu1.add_command(label="Régler l'affichage", command=self.adjustX)
         menu1.add_separator()
-        menu1.add_command(label="Quitter", command=self.root.quit)
+        menu1.add_command(label="Quitter", command=self.root.destroy)
         menubar.add_cascade(label="Configuration", menu=menu1)
 
         menu2 = Menu(menubar, tearoff=0)
@@ -55,6 +64,35 @@ class ServerGui():
     def alert(self):
         showinfo("alerte", "Bravo!")
 
+    # procédure de réglage de l'affichage
+    def adjustX(self):
+
+        def adjustX_valider():
+            i=0
+            self.xtext=int(entries[0].get())
+            self.xwip=int(entries[1].get())
+            self.xbox=int(entries[2].get())
+            self.xplan=int(entries[3].get())
+        
+        print("Réglages des variables d'affichage")
+        listeX=["X du texte","X de la valeur","X du voyant","X du plan"]
+        dialogueAdjustX = Tk()
+        dialogueAdjustX.title("Entrez les positions en X")
+        i=0
+        entries =[]
+        for line in listeX:
+            l = Label(dialogueAdjustX, text=line, width=10)
+            e = Entry(dialogueAdjustX, width=10)
+            l.grid(row=i, column=0)
+            e.grid(row=i, column=1)
+            entries.append(e)
+            i=i+1
+        b1 = Button(dialogueAdjustX, text='Valider', command=adjustX_valider)
+        b1.grid(row=i, column=0)
+        b2 = Button(dialogueAdjustX, text='Annuler', command=dialogueAdjustX.destroy)
+        b2.grid(row=i, column=1)
+        dialogueAdjustX.mainloop()
+        
         
     # Menu Configuration/ Initialiser les machines
     def initMachine(self) :
@@ -79,6 +117,7 @@ class ServerGui():
                 i=i+1
             db.commit()
             db.close()
+            #Il faut prévoir aussi l'écriture dans la base wipHistorique de ces mouvements
             
         dialogueConfigMachine = Tk()
         dialogueConfigMachine.title("Entrez la valeur des wip")
@@ -131,22 +170,31 @@ class ServerGui():
         cursor.execute("""SELECT machine, niveau, moment, alerte FROM etat""")
         rows = cursor.fetchall()
         i=0.5
+        # position en x des carrés de couleur
+        xbox=self.xbox
+        # position en x des noms de machines
+        xtext = self.xtext
+        # position en x des valeurs d'encours
+        xwip = self.xwip
+        # position en x du plan
+        xplan = self.xplan
+        
         for row in rows:
             print('{0} - {1} - {2} - {3}'.format(row[0], row[1], row[2], row[3]))
-            txt = self.canvas.create_text(75, i*self.hauteur/(nb+1), text=row[0], font="Arial 16 italic", fill="white")
-            txt2 = self.canvas.create_text(125, i*self.hauteur/(nb+1), text=row[1], font="Arial 16 italic", fill="yellow")
+            txt = self.canvas.create_text(xtext, i*self.hauteur/(nb+1), text=row[0], font="Arial 16 italic", fill="white")
+            txt2 = self.canvas.create_text(xwip, i*self.hauteur/(nb+1), text=row[1], font="Arial 16 italic", fill="yellow")
             if row[3] > row[1]:
-                self.canvas.create_rectangle(175-10, i*self.hauteur/(nb+1)-10,175+10, i*self.hauteur/(nb+1)+10,fill = 'green')
+                self.canvas.create_rectangle(xbox-10, i*self.hauteur/(nb+1)-10,xbox+10, i*self.hauteur/(nb+1)+10,fill = 'green')
             elif row[3] == row[1]:
-                self.canvas.create_rectangle(175-10, i*self.hauteur/(nb+1)-10,175+10, i*self.hauteur/(nb+1)+10,fill = 'orange')
+                self.canvas.create_rectangle(xbox-10, i*self.hauteur/(nb+1)-10,xbox+10, i*self.hauteur/(nb+1)+10,fill = 'orange')
             else:
-                self.canvas.create_rectangle(175-10, i*self.hauteur/(nb+1)-10,175+10, i*self.hauteur/(nb+1)+10,fill = 'red')
+                self.canvas.create_rectangle(xbox-10, i*self.hauteur/(nb+1)-10,xbox+10, i*self.hauteur/(nb+1)+10,fill = 'red')
             i=i+1
         db.close
 
         try:
             self.photo = PhotoImage(file="plan.gif")
-            self.canvas.create_image(260, 80, anchor=NW, image=self.photo)
+            self.canvas.create_image(xplan, 80, anchor=NW, image=self.photo)
             print("load img ok")
         except:
             print ("load img fail")
